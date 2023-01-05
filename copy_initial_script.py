bdeac35173005a774be18623bda660b11a8d1b05
feichay10 <alu0101364544@ull.edu.es>
2023-01-05 16:51:41 +0000

    Arreglos

--- copy_initial_script.py
+++ copy_initial_script.py
@@ -5,17 +5,44 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# Funcion para saber si la mano es derecha o izquierda
+# Funcion para saber si es la mano derecha o no
 def rightHand(thumb_x,pinky_x):
   if(thumb_x < pinky_x): # Comprueba la distancia x del pulgar al indice 
     return True
   return False
 
+def thumbOpenned(thumb_tip,thumb_bottom,pinky_x):
+  if(rightHand(thumb_tip,pinky_x)):       # Si es mano derecha
+    return (thumb_tip < thumb_bottom)    
+  else:                                   # Si es mano izquierda
+    return not (thumb_tip < thumb_bottom)
+
+# Comprueba que el pulgar este mirando hacia arriba y los dedos esten recogidos en un puño
+def thumb_up(lm):
+  return ((lm[2].y > lm[4].y) and (lm[7].y < lm[11].y) and (lm[11].y < lm[15].y) and (lm[15].y < lm[19].y)  
+            and (abs(lm[5].x - lm[6].x) < 0.05)  and (abs(lm[5].y - lm[6].y) < 0.05) and (abs(lm[4].x - lm[5].x) > 0.05)) 
+
+# Comprueba que el pulgar este mirando hacia abajo y los dedos esten recogidos en un puño
+def thumb_down(lm):
+  return ((lm[2].y < lm[4].y) and (lm[7].y > lm[11].y) and (lm[11].y > lm[15].y) and (lm[15].y > lm[19].y)
+            and (abs(lm[5].x - lm[6].x) < 0.05)  and (abs(lm[5].y - lm[6].y) < 0.05) and (abs(lm[4].x - lm[5].x) > 0.05)) 
+
+# Comprueba que los dedos indice y corazon esten juntos, el corazon este separado del anular y el anular este junto al meñique
+# def long_life(lm):
+#   return abs(lm[12].x-lm[8].x) < 0.08 and abs(lm[20].x-lm[16].x) < 0.08 and (abs(lm[16].x-lm[12].x) >= 0.08)
+
+# Comprueba que el indice este separado del corazon 
+def peace(lm):
+  return (lm[8].y < lm[6].y) and (lm[12].y < lm[10].y) and (abs(lm[12].x-lm[8].x) >= 0.08)
+ 
 cap = cv2.VideoCapture(0)
+coords_index = []
+edit_mode = False
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    min_tracking_confidence=0.5,
+    max_num_hands=3) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -36,9 +63,14 @@ with mp_hands.Hands(
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    
+    #height, width and depth (RGB=3) of image
+    (h,w,d) = image.shape
+  
     if results.multi_hand_landmarks:
+      hand_index = 0
+
       for hand_landmarks in results.multi_hand_landmarks:
+        count = 0
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
@@ -51,46 +83,39 @@ with mp_hands.Hands(
       #----------------
       
       # list of finger tips locators, 4 is thumb, 20 is pinky finger
-      tipIds = [4, 8, 12, 16, 20]
+        tipIds = [4, 8, 12, 16, 20]
       
-      lm = hand_landmarks.landmark
+        lm = hand_landmarks.landmark
       
       # x,y coordinates of pinky tip. Coordinates are normalized to [0.0,1.0] with width and height of the image
-      lm[tipIds[4]].x
-      lm[tipIds[4]].y
+        lm[tipIds[4]].x
+        lm[tipIds[4]].y
+      
+      # CONTAR LOS DEDOS LEVANTADOS
+        if(thumbOpenned(lm[tipIds[0]].x, lm[tipIds[0]-2].x, lm[tipIds[4]].x)): 
+            count = count + 1
 
-      #height, width and depth (RGB=3) of image
-      (h,w,d) = image.shape
+        fingers = 0
+        for i in range(0,5):
+          if i==0:
+            if lm[tipIds[i]].x < lm[tipIds[i]-1].x:
+              fingers += 1
+          else:
+            if lm[tipIds[i]].y < lm[tipIds[i]-1].y:
+              fingers += 1
+        cv2.putText(image, str(fingers), (550,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-      # Detecta mano derecha o izquierda
-      if(rightHand(thumb_tip,pinky_x)):       # Si es mano derecha
-        return (thumb_tip < thumb_bottom)    
-      else:                                   # Si es mano izquierda
-        return not (thumb_tip < thumb_bottom)
+      # ACTIVAR LIKE O DISLIKE
+        if lm[tipIds[0]].y < lm[tipIds[1]].y and lm[tipIds[1]].y < lm[tipIds[2]].y and lm[tipIds[2]].y < lm[tipIds[3]].y and lm[tipIds[3]].y < lm[tipIds[4]].y:
+          cv2.putText(image, "Like", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-      if((edit_mode == False) and thumb_up(lm)): 
-        edit_mode = True
-      # ACTIVAR MODO DIBUJO CON EL PULGAR HACIA ABAJO
-      elif((edit_mode == True) and thumb_down(lm)): 
-        edit_mode = False
-        coords_index.clear()
+        if lm[tipIds[0]].y > lm[tipIds[1]].y and lm[tipIds[1]].y > lm[tipIds[2]].y and lm[tipIds[2]].y > lm[tipIds[3]].y and lm[tipIds[3]].y > lm[tipIds[4]].y:
+          cv2.putText(image, "Dislike", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-      # Detecta like o dislike
-      # if lm[tipIds[0]].y < lm[tipIds[1]].y and lm[tipIds[1]].y < lm[tipIds[2]].y and lm[tipIds[2]].y < lm[tipIds[3]].y and lm[tipIds[3]].y < lm[tipIds[4]].y:
-      #   cv2.putText(image, "Like", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
-      # if lm[tipIds[0]].y > lm[tipIds[1]].y and lm[tipIds[1]].y > lm[tipIds[2]].y and lm[tipIds[2]].y > lm[tipIds[3]].y and lm[tipIds[3]].y > lm[tipIds[4]].y:
-      #   cv2.putText(image, "Dislike", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
-
-      # Contador de dedos
-      # fingers = 0
-      # for i in range(0,5):
-      #   if i==0:
-      #     if lm[tipIds[i]].x < lm[tipIds[i]-1].x:
-      #       fingers += 1
-      #   else:
-      #     if lm[tipIds[i]].y < lm[tipIds[i]-1].y:
-      #       fingers += 1
-      # cv2.putText(image, str(fingers), (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
+        # ACTIVAR GESTO DE SIMBOLO DE PAZ
+        if (count == 2):
+          if peace(lm):
+            cv2.putText(image, "Paz", (int(0.1*w),int(0.1*h)),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 3)
 
     cv2.imshow('MediaPipe Hands', image)    
     
--- initial_script.py
+++ initial_script.py
@@ -16,10 +16,10 @@ with mp_hands.Hands(
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
       continue
-    
+
     # Flip the image horizontally for a selfie-view display.
     image = cv2.flip(image, 1)
-    
+
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
@@ -29,8 +29,8 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-    
+
+
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -43,12 +43,12 @@ with mp_hands.Hands(
       #----------------
       # Detect fingers
       #----------------
-      
+
       # list of finger tips locators, 4 is thumb, 20 is pinky finger
       tipIds = [4, 8, 12, 16, 20]
-      
+
       lm = hand_landmarks.landmark
-      
+
       # x,y coordinates of pinky tip. Coordinates are normalized to [0.0,1.0] with width and height of the image
       lm[tipIds[4]].x
       lm[tipIds[4]].y
@@ -56,42 +56,35 @@ with mp_hands.Hands(
       #height, width and depth (RGB=3) of image
       (h,w,d) = image.shape
 
-      # OpenCV function to draw a circle:
-      #cv2.circle(image, center_coordinates, radius in pixels, color (Blue 0-255, Green 0-255, Red 0-255), thickness in pixels (-1 solid))
-      # Example: draw a red solid circle of 10 pixel radius in the tip of pinky finger:
-      # cv2.circle(image, (int(lm[tipIds[4]].x*w),int(lm[tipIds[4]].y*h)), 10, (0,0,255), -1)
+    # CONTAR LOS DEDOS LEVANTADOS
+      fingers = 0
+      fingers_aux = 0
+      for i in range(0,5):
+        if i==0:
+          if lm[tipIds[i]].x < lm[tipIds[i]-1].x:
+            fingers += 1
+            fingers_aux += 1
+        else:
+          if lm[tipIds[i]].y < lm[tipIds[i]-1].y:
+            fingers += 1
+            fingers_aux += 1
+      cv2.putText(image, str(fingers), (550,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-      # OpenCV function to draw text on image
-      #cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
-      # Example: draw a blue "hello" on the upper left corner of the image
-      # cv2.putText(image, "hello", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
-      # Dibuja linea negra
-      cv2.line(image, (int(lm[tipIds[0]].x*w),int(lm[tipIds[0]].y*h)), (int(lm[tipIds[4]].x*w),int(lm[tipIds[4]].y*h)), (0,0,0), 2)
-      cv2.countNonZero(cv2.circle(image, (int(lm[tipIds[4]].x*w),int(lm[tipIds[4]].y*h)), 10, (0,0,255), -1))
+    # ACTIVAR LIKE O DISLIKE
       if lm[tipIds[0]].y < lm[tipIds[1]].y and lm[tipIds[1]].y < lm[tipIds[2]].y and lm[tipIds[2]].y < lm[tipIds[3]].y and lm[tipIds[3]].y < lm[tipIds[4]].y:
         cv2.putText(image, "Like", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
       if lm[tipIds[0]].y > lm[tipIds[1]].y and lm[tipIds[1]].y > lm[tipIds[2]].y and lm[tipIds[2]].y > lm[tipIds[3]].y and lm[tipIds[3]].y > lm[tipIds[4]].y:
         cv2.putText(image, "Dislike", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
-      #OpenCV function to count the number of fingers up and draw a text on the image
-      # Fingers
-      # fingers = 0
-      # for i in range(0,5):
-      #   if i==0:
-      #     if lm[tipIds[i]].x < lm[tipIds[i]-1].x:
-      #       fingers += 1
-      #   else:
-      #     if lm[tipIds[i]].y < lm[tipIds[i]-1].y:
-      #       fingers += 1
-      # cv2.putText(image, str(fingers), (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-      # See other OpenCV functions to draw a line or a rectangle:
-      # cv2.line(image, start_point, end_point, color, thickness) 
-      # cv2.rectangle(image, start_point (top-left), end_point (bottom-right), color, thickness)
+    # ACTIVAR GESTO DE SIMBOLO DE PAZ
+      if (fingers_aux == 2):
+        if (lm[8].y < lm[6].y) and (lm[12].y < lm[10].y) and (abs(lm[12].x-lm[8].x) >= 0.08):
+          cv2.putText(image, "Peace", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
 
-    cv2.imshow('MediaPipe Hands', image)    
-    
+    cv2.imshow('MediaPipe Hands', image)
+
     if cv2.waitKey(5) & 0xFF == 27:
       break
-      
+
 cap.release()
